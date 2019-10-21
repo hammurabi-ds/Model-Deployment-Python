6828259a66bc85183f9ef9c345eca9b754c20c7b
EnumaElis <32635777+Hamurabbi@users.noreply.github.com>
2019-10-21 13:41:11 +0200

    Update message.py

--- message.py
+++ message.py
@@ -1,10 +1,10 @@
 import requests
 
-payload = {"data":"buy tickets last minute fast buy now and get two for free"}
+payload = {"data":"raw text example"}
 print(payload)
 headers=requests.utils.default_headers()
 
-prediction = requests.get("http://0.0.0.0:1000/predict", json=payload, verify=False, headers=headers)
+prediction = requests.get("http://0.0.0.0:PORT/predict", json=payload, verify=False, headers=headers)
 print(prediction.json())
 
 
