79c0da27664241644c73f00036bdf77473c6493a
Hamurabbi <azewain@gmail.com>
2020-11-25 14:28:51 +0100

    simpler version

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask
 from flask_restful import Api
-from resources.Predict import Predict
-from resources.Front import Front
+from resources.Predict import ServeModel
 
 app = Flask(__name__, template_folder='templates')
 api = Api(app)
 
-api.add_resource(Front, '/')
 api.add_resource(ServeModel, '/predict')
 
