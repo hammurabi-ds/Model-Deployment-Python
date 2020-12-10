df51a661445f76ba3ba73937454bc72b6a060106
Ahmed Zewain <32635777+Hamurabbi@users.noreply.github.com>
2020-12-10 09:43:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from resources.Predict import ServeModel
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(ServeModel, '/predict')
